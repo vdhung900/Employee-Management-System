68181ca1eddd0080f272117f1a62f80775c26774
vdhung900 <vodinhhung400@gmail.com>
2025-03-09 00:05:55 +0700

    CRUD attendance

---
+++ back-end/routes/attendanceRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const attendanceController = require('../controllers/attendanceController');
+
+// Mark attendance
+router.post('/', attendanceController.markAttendance);
+
+// Get employee attendance records
+router.get('/:employeeId', attendanceController.getEmployeeAttendance);
+
+// Generate attendance reports
+router.get('/reports', attendanceController.generateReports);
+
+// Update attendance record
+router.put('/:id', attendanceController.updateAttendance);
+
+// Delete attendance record
+router.delete('/:id', attendanceController.deleteAttendance);
+
+module.exports = router; 
