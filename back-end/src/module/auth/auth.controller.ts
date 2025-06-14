ec5a5a5332f2d050df7ad54d34668e7f2bd2091a
Quangz <quangzdrive>
2025-06-14 11:54:42 +0700

    fe: fix role login

--- back-end/src/module/auth/auth.controller.ts
+++ back-end/src/module/auth/auth.controller.ts
@@ -12,11 +12,11 @@ export class AuthController {
   @Get("/me")
   async getRequestMakerInfo(@Req() req: any): Promise<BaseResponse> {
     try {
-      console.log("awffaw", req.user);
+      // console.log("awffaw", req.user);
 
       const user = await this.authService.findUserById(req.user.userId);
 
-      console.log("awffaw", user);
+      // console.log("awffaw", user);
 
       return BaseResponse.success(user, "", HttpStatus.OK);
     } catch (e) {
@@ -39,7 +39,7 @@ export class AuthController {
   async login(@Body() loginReq: LoginReq): Promise<BaseResponse> {
     try {
       const jwtSecret = process.env.JWT_SECRET;
-      console.log("JWT Secret:", jwtSecret);
+      // console.log("JWT Secret:", jwtSecret);
 
       const loginResult = await this.authService.login(loginReq);
       return BaseResponse.success(loginResult, "Dang nhap thanh cong", HttpStatus.OK);
