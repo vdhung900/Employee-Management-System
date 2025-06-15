137eaa89e8577545f32299bb8126f5c212c255b5
TaiNgo30 <ngoductai2004@gmail.com>
2025-06-15 17:05:30 +0700

    add manager in department

--- back-end/src/module/department/department.module.ts
+++ back-end/src/module/department/department.module.ts
@@ -3,9 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Departments, DepartmentsSchema } from '../../schemas/departments.schema';
 import { DepartmentController } from './department.controller';
 import { DepartmentService } from './department.service';
+import { Account, AccountSchema } from '../../schemas/account.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Departments.name, schema: DepartmentsSchema }])],
+    imports: [
+        MongooseModule.forFeature([
+            { name: Departments.name, schema: DepartmentsSchema },
+            { name: Account.name, schema: AccountSchema }
+        ])
+    ],
     controllers: [DepartmentController],
     providers: [DepartmentService],
 })
