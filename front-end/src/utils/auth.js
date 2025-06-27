bb2915f575eeb81e76ca82e1cd98d50e14f08eda
Lưu Phúc Ân <anlp.sep19@gmail.com>
2025-06-27 21:56:57 +0700

    Update auth.js

--- front-end/src/utils/auth.js
+++ front-end/src/utils/auth.js
@@ -1,8 +1,18 @@
 import {jwtDecode} from "jwt-decode";
+import {Navigate} from "react-router-dom";
+import React from "react";
 
 export const getCurrentUser = () => {
   try {
     const user = localStorage.getItem("user");
+    const token = localStorage.getItem("accessToken");
+    const decoded = jwtDecode(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (decoded.exp && decoded.exp < currentTime) {
+      localStorage.clear();
+      let redirectTo = '/403';
+      return <Navigate to={redirectTo} replace state={{ unauthorized: true }} />;
+    }
     return user ? JSON.parse(user) : null;
   } catch (error) {
     console.error("Error getting current user:", error);
@@ -11,16 +21,17 @@ export const getCurrentUser = () => {
 };
 
 export const logout = () => {
-  localStorage.removeItem("user");
-  localStorage.removeItem("accessToken");
-  localStorage.removeItem("token");
-  localStorage.removeItem("refreshToken");
+  localStorage.clear();
 };
 
-export const login = (userData) => {
-  localStorage.setItem("user", JSON.stringify(userData));
-  localStorage.setItem("token", JSON.stringify(userData));
-  localStorage.setItem("role", "admin");
+export const login = (loginData) => {
+  const decode = jwtDecode(loginData.accessToken);
+  const permissionList = decode?.permissions.map(item => item.path);
+  localStorage.setItem("user", JSON.stringify(loginData.user));
+  localStorage.setItem("accessToken", loginData.accessToken || "");
+  localStorage.setItem("refreshToken", loginData.refreshToken || "");
+  localStorage.setItem("role", decode?.role.code);
+  localStorage.setItem("permissions", JSON.stringify(permissionList));
 };
 
 export const isAuthenticated = () => {
