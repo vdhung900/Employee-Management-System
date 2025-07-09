88db766f2f726a7b0786e87212b7ae158cd19b7d
vdhung900 <vodinhhung400@gmail.com>
2025-07-09 15:30:32 +0700

    code frontend for Manager Statistics

---
+++ front-end/src/services/ManagerStatisticsService.js
@@ -0,0 +1,20 @@
+import { fetchWithAuth, handleApiError } from "../utils/FetchWithAuth";
+
+const ManagerStatisticsService = {
+  async getEmployeesByDepartment(departmentId) {
+    try {
+      return await fetchWithAuth(`/employee/by-department/${departmentId}`);
+    } catch (e) {
+      throw handleApiError(e);
+    }
+  },
+  async getLeaveRequestsByDepartment(departmentId) {
+    try {
+      return await fetchWithAuth(`/request-manage/leave/by-department/${departmentId}`);
+    } catch (e) {
+      throw handleApiError(e);
+    }
+  }
+};
+
+export default ManagerStatisticsService; 
