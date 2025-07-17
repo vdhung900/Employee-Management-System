c5bba7c050aa2162b4c0ddea3530995af55ef636
anlp-dev <anlp.sep19@gmail.com>
2025-07-17 14:10:10 +0700

    update code

--- front-end/src/pages/employee/Request.js
+++ front-end/src/pages/employee/Request.js
@@ -49,7 +49,13 @@ import {
     BellOutlined,
     HistoryOutlined,
     CloseOutlined,
-    QuestionCircleOutlined
+    QuestionCircleOutlined,
+    CalendarOutlined,
+    PhoneOutlined,
+    TagOutlined,
+    RiseOutlined,
+    DollarOutlined,
+    StarOutlined
 } from '@ant-design/icons';
 import ThreeDContainer from '../../components/3d/ThreeDContainer';
 import CategoryService from "../../services/CategoryService";
@@ -1522,14 +1528,11 @@ const Requests = () => {
                                     valueStyle={{fontSize: '16px'}}
                                 />
                             </Col>
-                            {/*<Col span={12}>*/}
-                            {/*    <Statistic*/}
-                            {/*        title="Thời gian yêu cầu"*/}
-                            {/*        value={`${selectedRequest.startDate} - ${selectedRequest.endDate}`}*/}
-                            {/*        valueStyle={{ fontSize: '16px' }}*/}
-                            {/*    />*/}
-                            {/*</Col>*/}
                         </Row>
+
+                        {/* Hiển thị chi tiết dataReq theo loại yêu cầu */}
+                        <Divider orientation="left">Chi tiết yêu cầu</Divider>
+                        {renderRequestDetailByType(selectedRequest)}
 
                         <Divider orientation="left">Ghi chú</Divider>
                         <Paragraph>{selectedRequest.note}</Paragraph>
@@ -1563,5 +1566,160 @@ const Requests = () => {
         </div>
     );
 };
+
+// Hàm render chi tiết dataReq theo từng loại yêu cầu
+function renderRequestDetailByType(request) {
+    if (!request || !request.dataReq) return null;
+    const { typeRequest, dataReq } = request;
+    const labelStyle = { fontWeight: 'bold', marginRight: 8 };
+    const valueStyle = { marginBottom: 8 };
+    const { Text } = Typography;
+    const iconStyle = { marginRight: 6, color: '#722ed1' };
+    // Icon lớn đầu Card
+    const cardIconStyle = { fontSize: 32, color: '#722ed1', marginBottom: 8 };
+    let cardIcon = null;
+    switch (typeRequest.code) {
+        case 'LEAVE_REQUEST':
+        case 'MARRIAGE_LEAVE':
+        case 'MATERNITY_LEAVE':
+        case 'SICK_LEAVE':
+        case 'UNPAID_LEAVE':
+        case 'PATERNITY_LEAVE':
+        case 'REMOTE_WORK':
+        case 'FUNERAL_LEAVE':
+            cardIcon = <CalendarOutlined style={cardIconStyle}/>;
+            break;
+        case 'OVERTIME_REQUEST':
+            cardIcon = <ClockCircleOutlined style={cardIconStyle}/>;
+            break;
+        case 'ACCOUNT_CREATE_REQUEST':
+            cardIcon = <UserOutlined style={cardIconStyle}/>;
+            break;
+        case 'TARGET_REQUEST':
+            cardIcon = <StarOutlined style={cardIconStyle}/>;
+            break;
+        case 'SALARY_INCREASE':
+            cardIcon = <DollarOutlined style={cardIconStyle}/>;
+            break;
+        default:
+            cardIcon = <FileTextOutlined style={cardIconStyle}/>;
+    }
+    switch (typeRequest.code) {
+        case 'LEAVE_REQUEST':
+        case 'MARRIAGE_LEAVE':
+        case 'MATERNITY_LEAVE':
+        case 'SICK_LEAVE':
+        case 'UNPAID_LEAVE':
+        case 'PATERNITY_LEAVE':
+        case 'REMOTE_WORK':
+        case 'FUNERAL_LEAVE':
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12}>
+                            <Text strong><CalendarOutlined style={iconStyle}/>Ngày bắt đầu nghỉ:</Text>
+                            <br/>{dataReq.startDate ? formatDate(dataReq.startDate) : ''}
+                        </Col>
+                        <Col span={12}>
+                            <Text strong><CalendarOutlined style={iconStyle}/>Ngày kết thúc nghỉ:</Text>
+                            <br/>{dataReq.endDate ? formatDate(dataReq.endDate) : ''}
+                        </Col>
+                        <Col span={24}>
+                            <Text strong><FileTextOutlined style={iconStyle}/>Lý do:</Text>
+                            <br/>{dataReq.reason}
+                        </Col>
+                    </Row>
+                </Card>
+            );
+        case 'OVERTIME_REQUEST':
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12}>
+                            <Text strong><CalendarOutlined style={iconStyle}/>Ngày tăng ca:</Text>
+                            <br/>{dataReq.startDate ? formatDate(dataReq.startDate) : ''}
+                        </Col>
+                        <Col span={12}>
+                            <Text strong><ClockCircleOutlined style={iconStyle}/>Thời gian làm thêm:</Text>
+                            <br/>{Array.isArray(dataReq.hours) ? dataReq.hours.join(' - ') : ''}
+                        </Col>
+                        <Col span={24}>
+                            <Text strong><FileTextOutlined style={iconStyle}/>Lý do:</Text>
+                            <br/>{dataReq.reason}
+                        </Col>
+                    </Row>
+                </Card>
+            );
+        case 'ACCOUNT_CREATE_REQUEST':
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12}><Text strong><UserOutlined style={iconStyle}/>Họ và tên:</Text><br/>{dataReq.fullName}</Col>
+                        <Col span={12}><Text strong><CalendarOutlined style={iconStyle}/>Ngày bắt đầu làm việc:</Text><br/>{dataReq.startDate ? formatDate(dataReq.startDate) : ''}</Col>
+                        <Col span={12}><Text strong><MailOutlined style={iconStyle}/>Email:</Text><br/>{dataReq.email}</Col>
+                        <Col span={12}><Text strong><PhoneOutlined style={iconStyle}/>Số điện thoại:</Text><br/>{dataReq.phone}</Col>
+                        <Col span={12}><Text strong><TagOutlined style={iconStyle}/>Phòng ban:</Text><br/>{dataReq.department}</Col>
+                        <Col span={12}><Text strong><TagOutlined style={iconStyle}/>Chức vụ:</Text><br/>{dataReq.position}</Col>
+                        {dataReq.note && <Col span={24}><Text strong><FileTextOutlined style={iconStyle}/>Ghi chú:</Text><br/>{dataReq.note}</Col>}
+                        <Col span={24}><Text strong><FileTextOutlined style={iconStyle}/>Lý do:</Text><br/>{dataReq.reason}</Col>
+                    </Row>
+                </Card>
+            );
+        case 'TARGET_REQUEST':
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12}><Text strong><CalendarOutlined style={iconStyle}/>Tháng:</Text><br/>{dataReq.month}</Col>
+                        <Col span={12}><Text strong><CalendarOutlined style={iconStyle}/>Năm:</Text><br/>{dataReq.year}</Col>
+                        <Col span={24}>
+                            <Text strong><FileTextOutlined style={iconStyle}/>Mục tiêu:</Text>
+                            <List
+                                size="small"
+                                bordered
+                                style={{ marginTop: 8, background: '#fff' }}
+                                dataSource={dataReq.goals || []}
+                                renderItem={goal => (
+                                    <List.Item>
+                                        <Text strong>Tiêu đề:</Text> {goal.title} <Text strong>- Mức độ hoàn thành:</Text> {goal.targetValue}
+                                    </List.Item>
+                                )}
+                            />
+                        </Col>
+                        <Col span={24}><Text strong><FileTextOutlined style={iconStyle}/>Lý do:</Text><br/>{dataReq.reason}</Col>
+                    </Row>
+                </Card>
+            );
+        case 'SALARY_INCREASE':
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    <Row gutter={[8, 8]}>
+                        <Col span={12}><Text strong><UserOutlined style={iconStyle}/>Nhân viên:</Text><br/>{dataReq.employeeId}</Col>
+                        <Col span={12}><Text strong><TagOutlined style={iconStyle}/>Bộ phận:</Text><br/>{dataReq.department}</Col>
+                        <Col span={12}><Text strong><RiseOutlined style={iconStyle}/>Hệ số lương hiện tại:</Text><br/>{dataReq.currentCoefficient}</Col>
+                        <Col span={12}><Text strong><DollarOutlined style={iconStyle}/>Số lương hiện tại:</Text><br/>{dataReq.currentSalary}</Col>
+                        <Col span={12}><Text strong><RiseOutlined style={iconStyle}/>Hệ số lương đề xuất:</Text><br/>{dataReq.proposedCoefficient}</Col>
+                        <Col span={12}><Text strong><DollarOutlined style={iconStyle}/>Số lương đề xuất:</Text><br/>{dataReq.proposedSalary}</Col>
+                        <Col span={12}><Text strong><CalendarOutlined style={iconStyle}/>Ngày hiệu lực:</Text><br/>{dataReq.effectiveDate ? formatDate(dataReq.effectiveDate) : ''}</Col>
+                        <Col span={24}><Text strong><FileTextOutlined style={iconStyle}/>Lý do đề xuất:</Text><br/>{dataReq.reason}</Col>
+                    </Row>
+                </Card>
+            );
+        default:
+            // Hiển thị tất cả các trường còn lại nếu không khớp loại
+            return (
+                <Card size="small" bordered style={{ background: '#fafcff', marginBottom: 12 }}>
+                    <div style={{textAlign: 'center'}}>{cardIcon}</div>
+                    {Object.entries(dataReq).map(([key, value]) => (
+                        <div key={key} style={valueStyle}><Text strong>{key}:</Text> {String(value)}</div>
+                    ))}
+                </Card>
+            );
+    }
+}
 
 export default Requests;
