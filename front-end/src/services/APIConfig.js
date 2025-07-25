a2b4a361a5eacf5fed999c7f458af333eac149f8
TaiNgo30 <ngoductai2004@gmail.com>
2025-07-26 00:05:18 +0700

    hotfix

--- front-end/src/services/APIConfig.js
+++ front-end/src/services/APIConfig.js
@@ -2,17 +2,17 @@ const API_LOCAL = 'http://127.0.0.1:9123';
 const API_LOCAL_PROD = 'https://ems-api.api-score.com';
 
 const headers = {
-    'Content-Type': 'application/json',
+  'Content-Type': 'application/json',
 };
 
 const getAuthHeaders = (token) => ({
-    ...headers,
-    'Authorization': `Bearer ${token}`,
+  ...headers,
+  'Authorization': `Bearer ${token}`,
 });
 
 // khi code thi doi thanh local
 const APIConfig = {
-  baseUrl: API_LOCAL,
+  baseUrl: API_LOCAL_PROD,
   headers,
   getAuthHeaders,
 };
